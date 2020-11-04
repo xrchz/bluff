bb0a6632a050ffd6ea36c025c5256773f415b2ee
Ramana Kumar <ramana@member.fsf.org>
2020-11-04 17:53:32 +0000

    Start on tracewords

---
+++ client/trace.js
@@ -0,0 +1,145 @@
+/* global io */
+var socket = io("https://xrchz.net", {path: '/games/trace/socket.io'})
+
+const gameInput = document.getElementById('game')
+const nameInput = document.getElementById('name')
+const errorMsg = document.getElementById('errorMsg')
+const gamesList = document.getElementById('games')
+const joinButton = document.getElementById('join')
+const startButton = document.getElementById('start')
+const spectateInput = document.getElementById('spectate')
+const spectatorsDiv = document.getElementById('spectators')
+const unseated = document.getElementById('unseated')
+const playArea = document.getElementById('playArea')
+const letterGrid = document.getElementById('letterGrid')
+const resultsArea = document.getElementById('resultsArea')
+
+const fragment = document.createDocumentFragment()
+
+joinButton.parentElement.onsubmit = () => {
+  socket.emit('joinRequest', {
+    gameName:  gameInput.value.toUpperCase().replace(/[^A-Z]/g, '').substring(0, 2),
+    playerName: nameInput.value.replace(/\W/g, ''),
+    spectate: spectateInput.checked
+  })
+  return false
+}
+
+startButton.onclick = () => socket.emit('startGame')
+
+socket.on('ensureLobby', () => {
+  errorMsg.innerHTML = ''
+  gameInput.disabled = false
+  nameInput.disabled = false
+  joinButton.hidden = false
+  spectateInput.hidden = false
+  spectateInput.previousElementSibling.hidden = false
+  spectateInput.disabled = false
+  unseated.innerHTML = ''
+  startButton.hidden = true
+  spectatorsDiv.innerHTML = ''
+  playArea.hidden = true
+  letterGrid.innerHTML = ''
+  resultsArea.innerHTML = ''
+  resultsArea.hidden = true
+})
+
+socket.on('updateGames', games => {
+  gamesList.innerHTML = ''
+  for (const game of games) {
+    const li = fragment.appendChild(document.createElement('li'))
+    let a = li.appendChild(document.createElement('a'))
+    a.textContent = game.name
+    a.onclick = () => {
+      gameInput.value = gameInput.value === game.name ? '' : game.name
+    }
+    const ul = li.appendChild(document.createElement('ul'))
+    ul.classList.add('inline')
+    for (const player of game.players) {
+      a = ul.appendChild(document.createElement('li'))
+      if (player.disconnected) {
+        a = a.appendChild(document.createElement('a'))
+        a.classList.add('disconnected')
+        a.onclick = () => {
+          if (gameInput.value === game.name && nameInput.value === player.name)
+            nameInput.value = ''
+          else {
+            gameInput.value = game.name
+            nameInput.value = player.name
+          }
+        }
+      }
+      a.textContent = player.name
+    }
+  }
+  gamesList.appendChild(fragment)
+  gamesList.hidden = !games.length
+})
+
+socket.on('updateUnseated', players => {
+  unseated.innerHTML = ''
+  let elem
+  for (player of players) {
+    elem = document.createElement('li')
+    elem.textContent = player.name
+    unseated.appendChild(elem)
+  }
+  errorMsg.innerHTML = ''
+})
+
+socket.on('updateSpectators', spectators => {
+  spectatorsDiv.innerHTML = ''
+  let elem
+  if (spectators.length) {
+    spectators.unshift({ name: 'Spectators:' })
+  }
+  for (spectator of spectators) {
+    elem = document.createElement('li')
+    elem.textContent = spectator.name
+    spectatorsDiv.appendChild(elem)
+  }
+})
+
+socket.on('joinedGame', data => {
+  gameInput.value = data.gameName
+  nameInput.value = data.playerName
+  spectateInput.checked = data.spectating
+  gameInput.disabled = true
+  nameInput.disabled = true
+  spectateInput.disabled = true
+  if (!spectateInput.checked) {
+    spectateInput.previousElementSibling.hidden = true
+    spectateInput.hidden = true
+  }
+  joinButton.hidden = true
+  if (!spectateInput.checked)
+    startButton.hidden = false
+  errorMsg.innerHTML = ''
+})
+
+socket.on('gameStarted', grid => {
+  startButton.hidden = true
+  let i = 0
+  while (i < grid.length) {
+    const div = fragment.appendChild(document.createElement('div'))
+    div.id = `g${i}`
+    div.textContent = grid[i++]
+  }
+  letterGrid.appendChild(fragment)
+  playArea.hidden = false
+  errorMsg.innerHTML = ''
+})
+
+socket.on('listWords', words => {
+  resultsArea.hidden = false
+  const ul = fragment.appendChild(document.createElement('ul'))
+  for (const wp of words) {
+    const li = ul.appendChild(document.createElement('li'))
+    li.textContent = wp[0]
+  }
+  resultsArea.appendChild(fragment)
+})
+
+socket.on('errorMsg', msg => {
+  errorMsg.innerHTML = msg
+})
---
+++ trace.js
@@ -0,0 +1,325 @@
+'use strict'
+
+const express = require('express')
+const http = require('http')
+const fs = require('fs')
+var app = express()
+var server = http.createServer(app)
+var io = require('socket.io')(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/client/trace.html`)
+})
+app.use(express.static(`${__dirname}/client`))
+
+const unix = '/run/games/trace.socket'
+server.listen(unix)
+console.log(`server started on ${unix}`)
+server.on('listening', () => fs.chmodSync(unix, 0o777))
+process.on('SIGINT', () => { fs.unlinkSync(unix); process.exit() })
+
+const wordTrie = { sub: {} }
+for (const word of fs.readFileSync('trace.txt', 'utf8').split('\n')) {
+  if (word.length < 3) continue
+  let prev, node = wordTrie
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i]
+    if (node.sub[letter] === undefined)
+      node.sub[letter] = { sub: {} }
+    if (letter === 'q') prev = node
+    node = node.sub[letter]
+    if (letter === 'u' && 0 < i && word[i-1] === 'q')
+      prev.sub['qu'] = node
+  }
+  node.end = true
+}
+/*
+x
+x.sub['a'] = y
+y.sub['q'] = z
+z.sub['u'] = w
+w.sub['a'] = v
+v.end
+want: y.sub['qu'] = v
+i = 0, node = x
+i = 1, node = y, prev = y
+i = 2, node = w, prev = y
+*/
+
+function findAllWords(grid) {
+  const words = new Map()
+  function addWords(path, prefix, node) {
+    if (node.end && !words.has(prefix)) words.set(prefix, path)
+    const pos = path[path.length - 1]
+    const col = pos % 4
+    const row = (pos - col) / 4
+    for (const coldelta of [-1, 0, 1]) {
+      for (const rowdelta of [-1, 0, 1]) {
+        if (coldelta === 0 && rowdelta === 0) continue
+        if (col + coldelta < 0 || col + coldelta >= 4) continue
+        if (row + rowdelta < 0 || row + rowdelta >= 4) continue
+        const newpos = (row + rowdelta) * 4 + col + coldelta
+        if (path.includes(newpos)) continue
+        const letter = grid[newpos]
+        if (node.sub[letter] === undefined) continue
+        const newpath = Array.from(path)
+        newpath.push(newpos)
+        addWords(newpath, prefix + letter, node.sub[letter])
+      }
+    }
+  }
+  for (let pos = 0; pos < 16; pos++) {
+    const letter = grid[pos]
+    const node = wordTrie.sub[letter]
+    if (node) addWords([pos], letter, node)
+  }
+  return words
+}
+
+const hardDice = [
+  'aaciot', 'ahmors', 'egkluy', 'abilty',
+  'acdemp', 'egintv', 'gilruw', 'elpstu',
+  'denosw', 'acelrs', 'abjmoq', 'eefhiy',
+  'ehinps', 'dknotu', 'adenvz', 'biforx']
+
+const easyDice = [
+  'aaeegn', 'elrtty', 'aoottw', 'abbjoo',
+  'ehrtvw', 'cimotu', 'distty', 'eiosst',
+  'delrvy', 'achops', 'himnqu', 'eeinsu',
+  'eeghnw', 'affkps', 'hlnnrz', 'deilrx']
+
+function shuffleInPlace(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i)
+    const t = array[i]
+    array[i] = array[j]
+    array[j] = t
+  }
+}
+
+/*
+const letterFreqs = []
+for (let i = 0; i <= 26; i++) letterFreqs[i] = 0
+for (const word of wordList) {
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === 'q' && i+1 < word.length && word[i+1] === 'u') {
+      letterFreqs[26]++
+      i++
+    }
+    else
+      letterFreqs[word.charCodeAt(i) - 97]++
+  }
+}
+console.log(letterFreqs)
+const logBase = 1 / Math.log(360)
+for (let i = 0; i <= 26; i++) letterFreqs[i] = logBase * Math.log(letterFreqs[i])
+console.log(letterFreqs)
+const logFreqTotal = letterFreqs.reduce((t, n) => t + n)
+console.log(logFreqTotal)
+
+function randomGridLetter() {
+  const r = Math.random() * logFreqTotal
+  let t = 0
+  for (let i = 0; i <= 26; i++) {
+    t += letterFreqs[i]
+    if (t >= r) {
+      return i < 26 ? String.fromCharCode(i + 97) : 'qu'
+    }
+  }
+}
+for (let i = 0; i < 16; i++)
+  console.log(randomGridLetter())
+*/
+
+function randomGrid(origDice) {
+  const dice = Array.from(origDice)
+  shuffleInPlace(dice)
+  return dice.map(die => {
+    const l = die[Math.floor(Math.random() * 6)]
+    return l === 'q' ? 'qu' : l
+  })
+}
+
+const games = {}
+
+const randomLetter = () => String.fromCharCode(65 + Math.random() * 26)
+
+function randomUnusedGameName() {
+  if (Object.keys(games).length === 26 * 26) {
+    console.log('all game names in use')
+    return 'Overflow'
+  }
+  let name
+  do { name = randomLetter() + randomLetter() } while (name in games)
+  return name
+}
+
+function updateGames(room) {
+  if (!room) room = 'lobby'
+  const data = []
+  for (const [gameName, game] of Object.entries(games))
+    data.push({ name: gameName,
+                players: game.players.map(player => ({ name: player.name, disconnected: !player.socketId }))
+              })
+  io.in(room).emit('updateGames', data)
+}
+
+io.on('connection', socket => {
+  console.log(`new connection ${socket.id}`)
+
+  socket.emit('ensureLobby')
+  socket.join('lobby'); updateGames(socket.id)
+
+  socket.on('joinRequest', data => {
+    let game
+    let gameName = data.gameName
+    if (!gameName) gameName = randomUnusedGameName()
+    if (!(gameName in games)) {
+      console.log(`new game ${gameName}`)
+      game = {
+        players: [],
+        spectators: [],
+        timeLimit: 180
+      }
+      games[gameName] = game
+    }
+    else
+      game = games[gameName]
+    if (!data.playerName) {
+      socket.playerName = `Finger${Math.floor(Math.random()*20)}`
+      console.log(`random name ${socket.playerName} for ${socket.id}`)
+    }
+    else {
+      socket.playerName = data.playerName
+      console.log(`name ${socket.playerName} supplied for ${socket.id}`)
+    }
+    if (data.spectate) {
+      if (game.spectators.every(spectator => spectator.name !== socket.playerName)) {
+        console.log(`${socket.playerName} joining ${gameName} as spectator`)
+        socket.gameName = gameName
+        socket.leave('lobby'); socket.emit('updateGames', [])
+        socket.join(gameName)
+        game.spectators.push({ socketId: socket.id, name: socket.playerName })
+        socket.emit('joinedGame',
+          { gameName: gameName, playerName: socket.playerName, spectating: true })
+        // updateSettings(game, socket.id)
+        io.in(gameName).emit('updateSpectators', game.spectators)
+        if (!game.started) {
+          socket.emit('updateUnseated', game.players)
+        }
+        else {
+          socket.emit('gameStarted', game.grid)
+          // reconnection
+        }
+      }
+      else {
+        console.log(`${socket.playerName} barred from joining ${gameName} as duplicate spectator`)
+        socket.emit('errorMsg', `Game ${gameName} already contains spectator ${socket.playerName}.`)
+      }
+    }
+    else if (game.started) {
+      if (game.players.find(player => player.name === socket.playerName && !player.socketId)) {
+        const rooms = Object.keys(socket.rooms)
+        if (rooms.length === 2 && rooms.includes(socket.id) && rooms.includes('lobby')) {
+          console.log(`${socket.playerName} rejoining ${gameName}`)
+          socket.gameName = gameName
+          socket.leave('lobby'); socket.emit('updateGames', [])
+          socket.join(gameName)
+          const player = game.players.find(player => player.name === socket.playerName)
+          player.socketId = socket.id
+          socket.emit('joinedGame', { gameName: gameName, playerName: socket.playerName })
+          // updateSettings(game, socket.id)
+          socket.emit('updateSpectators', game.spectators)
+          socket.emit('gameStarted', game.grid)
+          // reconnection
+        }
+        else {
+          console.log(`error: ${socket.playerName} rejoining ${gameName} while in ${rooms}`)
+          socket.emit('errorMsg', 'Error: somehow this connection is already used in another game.')
+        }
+      }
+      else {
+        console.log(`${socket.playerName} barred from joining ${gameName} as extra player`)
+        socket.emit('errorMsg', `Game ${gameName} has already started. Try spectating.`)
+      }
+    }
+    else {
+      if (game.players.every(player => player.name !== socket.playerName)) {
+        console.log(`${socket.playerName} joining ${gameName}`)
+        socket.leave('lobby'); socket.emit('updateGames', [])
+        socket.join(gameName)
+        socket.gameName = gameName
+        game.players.push({ socketId: socket.id, name: socket.playerName })
+        socket.emit('joinedGame', { gameName: gameName, playerName: socket.playerName })
+        // updateSettings(game, socket.id)
+        socket.emit('updateSpectators', game.spectators)
+        io.in(gameName).emit('updateUnseated', game.players)
+      }
+      else {
+        console.log(`${socket.playerName} barred from joining ${gameName} as duplicate player`)
+        socket.emit('errorMsg', `Game ${gameName} already contains player ${socket.playerName}.`)
+      }
+    }
+    updateGames()
+  })
+
+  function inGame(func) {
+    const gameName = socket.gameName
+    const game = games[gameName]
+    if (game) func(gameName, game)
+    else {
+      console.log(`${socket.playerName} failed to find game ${gameName}`)
+      socket.emit('errorMsg', `Game ${gameName} not found. Try reconnecting.`)
+    }
+  }
+
+  socket.on('startGame', () => inGame((gameName, game) => {
+    if (!game.started) {
+      if (game.players.length) {
+        console.log(`starting ${gameName}`)
+        game.started = true
+        game.grid = randomGrid(hardDice)
+        game.words = findAllWords(game.grid)
+        io.in(gameName).emit('gameStarted', game.grid)
+        io.in(gameName).emit('listWords', Array.from(game.words))
+      }
+      else {
+        socket.emit('errorMsg', 'Error: not enough players to start.')
+      }
+    }
+    else {
+      console.log(`${socket.playerName} attempted to start ${gameName} again`)
+      socket.emit('errorMsg', `Error: ${gameName} has already started.`)
+    }
+  }))
+
+  socket.on('disconnecting', () => {
+    console.log(`${socket.playerName} exiting ${socket.gameName}`)
+    const gameName = socket.gameName
+    const game = games[gameName]
+    if (game) {
+      if (!game.started) {
+        const notThisPlayer = player => player.socketId !== socket.id
+        game.players = game.players.filter(notThisPlayer)
+        game.spectators = game.spectators.filter(notThisPlayer)
+        io.in(gameName).emit('updateSpectators', game.spectators)
+        io.in(gameName).emit('updateUnseated', game.players)
+        if (game.players.length === 0 && game.spectators.length === 0) {
+          console.log(`removing empty game ${gameName}`)
+          delete games[gameName]
+        }
+      }
+      else {
+        const spectators = game.spectators.filter(player => player.socketId !== socket.id)
+        if (spectators.length < game.spectators.length) {
+          game.spectators = spectators
+          io.in(gameName).emit('updateSpectators', game.spectators)
+        }
+        else {
+          game.players.find(player => player.socketId === socket.id).socketId = null
+        }
+      }
+      updateGames()
+    }
+  })
+})
