520c07bf5324c2cca1670b3c09363d393d76af03
Ramana Kumar <ramana@member.fsf.org>
2021-02-25 22:01:07 +0000

    Add initial implementation of arrow

---
+++ arrow.js
@@ -0,0 +1,331 @@
+'use strict'
+
+const express = require('express')
+const http = require('http')
+const fs = require('fs')
+var app = express()
+var server = http.createServer(app)
+var io = require('socket.io')(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/client/arrow.html`)
+})
+app.use(express.static(`${__dirname}/client`))
+
+const unix = '/run/games/arrow.socket'
+server.listen(unix)
+console.log(`server started on ${unix}`)
+server.on('listening', () => fs.chmodSync(unix, 0o777))
+
+const saveFile = 'arrow.json'
+
+const games = JSON.parse(fs.readFileSync(saveFile, 'utf8'))
+
+function saveGames() {
+  let toSave = {}
+  for (const [gameName, game] of Object.entries(games))
+    if (game.started) toSave[gameName] = game
+  fs.writeFileSync(saveFile,
+    JSON.stringify(
+      toSave,
+      (k, v) => k === 'socketId' ? null :
+                k === 'spectators' ? [] : v))
+}
+
+process.on('SIGINT', () => { saveGames(); fs.unlinkSync(unix); process.exit() })
+process.on('uncaughtExceptionMonitor', saveGames)
+
+const randomLetter = () => String.fromCharCode(65 + Math.random() * 26)
+
+function randomUnusedGameName() {
+  if (Object.keys(games).length === 26 * 26) {
+    console.log('all game names in use')
+    return 'Overflow'
+  }
+  let name
+  do { name = randomLetter() + randomLetter() } while (name in games)
+  return name
+}
+
+function updateGames(room) {
+  if (!room) room = 'lobby'
+  const data = []
+  for (const [gameName, game] of Object.entries(games))
+    data.push({ name: gameName,
+                players: game.players.map(player => ({ name: player.name, disconnected: !player.socketId }))
+              })
+  io.in(room).emit('updateGames', data)
+}
+
+function makePieces() {
+  const pieces = []
+  for (let d0 = 0; d0 < 2; d0++)
+    for (let d1 = 0; d1 < 2; d1++)
+      for (let d2 = 0; d2 < 2; d2++)
+        for (let d3 = 0; d3 < 2; d3++)
+          pieces.push({d: [d0, d1, d2, d3]})
+  return pieces
+}
+
+function lineIndex(x, i, j) {
+  if (x === 0) return 4 * i + j
+  if (x === 1) return 4 * j + i
+  if (x === 2 && i === 0) return 4 * j + j
+  return 4 * j + (3 - j)
+}
+
+function markArrow(board) {
+  for (let x = 0; x < 3; x++) {
+    const imax = x < 2 ? 4 : 2
+    for (let i = 0; i < imax; i++) {
+      let ds = [0, 0, 0, 0]
+      let j, d
+      for (j = 0; j < 4; j++) {
+        const k = lineIndex(x, i, j)
+        if (!board[k]) { j = 5; break }
+        for (d = 0; d < 4; d++)
+          ds[d] += board[k].d[d]
+      }
+      if (j === 5) continue
+      for (d = 0; d < 4; d++) {
+        if (ds[d] === 0 || ds[d] === 4) {
+          for (j = 0; j < 4; j++) {
+            const k = lineIndex(x, i, j)
+            board[k].arrow = true
+          }
+          return true
+        }
+      }
+    }
+  }
+  return false
+}
+
+io.on('connection', socket => {
+  console.log(`new connection ${socket.id}`)
+
+  socket.emit('ensureLobby')
+  socket.join('lobby'); updateGames(socket.id)
+
+  socket.on('joinRequest', data => {
+    let game
+    let gameName = data.gameName
+    if (!gameName) gameName = randomUnusedGameName()
+    if (!(gameName in games)) {
+      console.log(`new game ${gameName}`)
+      game = { players: [], spectators: [] }
+      games[gameName] = game
+    }
+    else
+      game = games[gameName]
+    if (!data.playerName) {
+      socket.playerName = `Archer${Math.floor(Math.random()*20)}`
+      console.log(`random name ${socket.playerName} for ${socket.id}`)
+    }
+    else {
+      socket.playerName = data.playerName
+      console.log(`name ${socket.playerName} supplied for ${socket.id}`)
+    }
+    if (data.spectate) {
+      if (game.spectators.every(spectator => spectator.name !== socket.playerName)) {
+        console.log(`${socket.playerName} joining ${gameName} as spectator`)
+        socket.gameName = gameName
+        socket.leave('lobby'); socket.emit('updateGames', [])
+        socket.join(gameName)
+        socket.join(`${gameName}spectators`)
+        game.spectators.push({ socketId: socket.id, name: socket.playerName })
+        socket.emit('joinedGame',
+          { gameName: gameName, playerName: socket.playerName, spectating: true })
+        io.in(gameName).emit('updateSpectators', game.spectators)
+        socket.emit('updatePlayers', game.players)
+        if (game.started) {
+          socket.emit('gameStarted')
+          socket.emit('updateBoard', game.board)
+          socket.emit('updatePieces', { pieces: game.pieces })
+        }
+      }
+      else {
+        console.log(`${socket.playerName} barred from joining ${gameName} as duplicate spectator`)
+        socket.emit('errorMsg', `Game ${gameName} already contains spectator ${socket.playerName}.`)
+      }
+    }
+    else if (game.started) {
+      if (game.players.find(player => player.name === socket.playerName && !player.socketId)) {
+        const rooms = Object.keys(socket.rooms)
+        if (rooms.length === 2 && rooms.includes(socket.id) && rooms.includes('lobby')) {
+          console.log(`${socket.playerName} rejoining ${gameName}`)
+          socket.gameName = gameName
+          socket.leave('lobby'); socket.emit('updateGames', [])
+          socket.join(gameName)
+          const player = game.players.find(player => player.name === socket.playerName)
+          player.socketId = socket.id
+          socket.emit('joinedGame', { gameName: gameName, playerName: socket.playerName })
+          socket.emit('updateSpectators', game.spectators)
+          io.in(gameName).emit('updatePlayers', game.players)
+          socket.emit('gameStarted')
+          socket.emit('updateBoard', game.board)
+          const data = { pieces: game.pieces }
+          const current = game.players.find(player => player.current)
+          if (current) data.currentPlayer = current.name
+          socket.emit('updatePieces', data)
+          if (game.undoLog.length) socket.emit('showUndo', true)
+        }
+        else {
+          console.log(`error: ${socket.playerName} rejoining ${gameName} while in ${rooms}`)
+          socket.emit('errorMsg', 'Error: somehow this connection is already used in another game.')
+        }
+      }
+      else {
+        console.log(`${socket.playerName} barred from joining ${gameName} as extra player`)
+        socket.emit('errorMsg', `Game ${gameName} has already started. Try spectating.`)
+      }
+    }
+    else {
+      if (game.players.every(player => player.name !== socket.playerName)) {
+        console.log(`${socket.playerName} joining ${gameName}`)
+        socket.leave('lobby'); socket.emit('updateGames', [])
+        socket.join(gameName)
+        socket.gameName = gameName
+        game.players.push({ socketId: socket.id, name: socket.playerName })
+        socket.emit('joinedGame', { gameName: gameName, playerName: socket.playerName })
+        socket.emit('updateSpectators', game.spectators)
+        io.in(gameName).emit('updatePlayers', game.players)
+      }
+      else {
+        console.log(`${socket.playerName} barred from joining ${gameName} as duplicate player`)
+        socket.emit('errorMsg', `Game ${gameName} already contains player ${socket.playerName}.`)
+      }
+    }
+    updateGames()
+  })
+
+  function inGame(func) {
+    const gameName = socket.gameName
+    const game = games[gameName]
+    if (game) func(gameName, game)
+    else {
+      console.log(`${socket.playerName} failed to find game ${gameName}`)
+      socket.emit('errorMsg', `Game ${gameName} not found. Try reconnecting.`)
+    }
+  }
+
+  socket.on('startGame', () => inGame((gameName, game) => {
+    if (!game.started) {
+      if (game.players.length) {
+        console.log(`starting ${gameName}`)
+        game.started = true
+        game.undoLog = []
+        game.pieces = makePieces()
+        game.board = []
+        for (let i = 0; i < 16; i++) game.board[i] = null
+        const current = game.players[Math.floor(Math.random() * game.players.length)]
+        current.current = true
+        io.in(gameName).emit('gameStarted')
+        io.in(gameName).emit('updatePlayers', game.players)
+        io.in(gameName).emit('updateBoard', game.board)
+        io.in(gameName).emit('updatePieces', { pieces: game.pieces, currentPlayer: current.name })
+      }
+      else {
+        socket.emit('errorMsg', 'Error: not enough players to start.')
+      }
+    }
+    else {
+      console.log(`${socket.playerName} attempted to start ${gameName} again`)
+      socket.emit('errorMsg', `Error: ${gameName} has already started.`)
+    }
+  }))
+
+  socket.on('playRequest', data => inGame((gameName, game) => {
+    if (game.started && !game.ended) {
+      const currentIndex = game.players.findIndex(player => player.socketId === socket.id)
+      if (0 <= currentIndex) {
+        const player = game.players[currentIndex]
+        if (player.current &&
+            Number.isInteger(data.pieceIndex) && 0 <= data.pieceIndex && data.pieceIndex < game.pieces.length &&
+            Number.isInteger(data.placeIndex) && 0 <= data.placeIndex && data.placeIndex < 16 &&
+            game.board[data.placeIndex] === null) {
+          const piece = game.pieces.splice(data.pieceIndex, 1)[0]
+          game.board[data.placeIndex] = piece
+          let nextPlayer
+          if (markArrow(game.board)) {
+            game.ended = true
+            delete player.current
+            player.winner = true
+            io.in(gameName).emit('updatePlayers', game.players)
+          }
+          else {
+            delete player.current
+            let nextIndex = currentIndex + 1
+            if (nextIndex === game.players.length) nextIndex = 0
+            nextPlayer = game.players[nextIndex]
+            nextPlayer.current = true
+          }
+          io.in(gameName).emit('updateBoard', game.board)
+          data = { pieces: game.pieces }
+          if (nextPlayer) data.currentPlayer = nextPlayer.name
+          io.in(gameName).emit('updatePieces', data)
+        }
+        else {
+          console.log(`${socket.playerName} played invalid data or is not current`)
+          socket.emit('errorMsg', `Error: invalid play data.`)
+        }
+      }
+      else {
+        console.log(`${socket.playerName} not found as player in ${gameName}`)
+        socket.emit('errorMsg', `Error: could not find you as a player.`)
+      }
+    }
+    else {
+      console.log(`${socket.playerName} playing in not active ${gameName}`)
+      socket.emit('errorMsg', `Error: ${gameName} has not started or has finished.`)
+    }
+  }))
+
+  // TODO: undoRequest
+
+  socket.on('disconnecting', () => {
+    console.log(`${socket.playerName} exiting ${socket.gameName}`)
+    const gameName = socket.gameName
+    const game = games[gameName]
+    if (game) {
+      if (!game.started) {
+        const notThisPlayer = player => player.socketId !== socket.id
+        game.players = game.players.filter(notThisPlayer)
+        game.spectators = game.spectators.filter(notThisPlayer)
+        io.in(gameName).emit('updateSpectators', game.spectators)
+        io.in(gameName).emit('updatePlayers', game.players)
+        if (game.players.length === 0 && game.spectators.length === 0) {
+          console.log(`removing empty game ${gameName}`)
+          delete games[gameName]
+        }
+      }
+      else {
+        const spectators = game.spectators.filter(player => player.socketId !== socket.id)
+        if (spectators.length < game.spectators.length) {
+          game.spectators = spectators
+          io.in(gameName).emit('updateSpectators', game.spectators)
+        }
+        else {
+          const player = game.players.find(player => player.socketId === socket.id)
+          if (player) {
+            player.socketId = null
+            io.in(gameName).emit('updatePlayers', game.players)
+          }
+          if (game.timeout && game.players.every(player => !player.socketId)) {
+            console.log(`pausing ${gameName} since all players are disconnected`)
+            clearTimeout(game.timeout)
+            delete game.timeout
+          }
+        }
+      }
+      updateGames()
+    }
+  })
+
+  socket.on('deleteGame', gameName => {
+    delete games[gameName]
+    updateGames()
+  })
+
+  socket.on('saveGames', saveGames)
+})
---
+++ client/arrow.js
@@ -0,0 +1,215 @@
+/* global io */
+var socket = io("https://xrchz.net", {path: '/games/arrow/socket.io'})
+
+const gameInput = document.getElementById('game')
+const nameInput = document.getElementById('name')
+const errorMsg = document.getElementById('errorMsg')
+const gamesList = document.getElementById('games')
+const joinButton = document.getElementById('join')
+const startButton = document.getElementById('start')
+const undoButton = document.getElementById('undo')
+const playersList = document.getElementById('players')
+const spectateInput = document.getElementById('spectate')
+const spectatorsList = document.getElementById('spectators')
+const playArea = document.getElementById('playArea')
+const noMatchesButton = document.getElementById('noMatches')
+const boardDiv = document.getElementById('board')
+const piecesDiv = document.getElementById('pieces')
+
+const fragment = document.createDocumentFragment()
+
+joinButton.parentElement.onsubmit = () => {
+  socket.emit('joinRequest', {
+    gameName:  gameInput.value.toUpperCase().replace(/[^A-Z]/g, '').substring(0, 2),
+    playerName: nameInput.value.replace(/\W/g, ''),
+    spectate: spectateInput.checked
+  })
+  return false
+}
+
+undoButton.onclick = () => {
+  socket.emit('undoRequest')
+  errorMsg.innerHTML = ''
+}
+
+startButton.onclick = () => socket.emit('startGame')
+
+socket.on('ensureLobby', () => {
+  errorMsg.innerHTML = ''
+  gameInput.disabled = false
+  nameInput.disabled = false
+  joinButton.hidden = false
+  spectateInput.hidden = false
+  spectateInput.previousElementSibling.hidden = false
+  spectateInput.disabled = false
+  startButton.hidden = true
+  undoButton.hidden = true
+  playersList.innerHTML = ''
+  spectatorsList.innerHTML = ''
+  playArea.hidden = true
+  boardDiv.innerHTML = ''
+  piecesDiv.innerHTML = ''
+  history.replaceState('lobby', 'Lobby')
+})
+
+window.onpopstate = function (e) {
+  if (e.state === 'lobby') {
+    socket.close()
+    socket.open()
+  }
+  else if (e.state)
+    socket.emit('joinRequest', e.state)
+}
+
+socket.on('updateGames', games => {
+  gamesList.innerHTML = ''
+  for (const game of games) {
+    const li = fragment.appendChild(document.createElement('li'))
+    let a = li.appendChild(document.createElement('a'))
+    a.textContent = game.name
+    a.onclick = () => {
+      gameInput.value = gameInput.value === game.name ? '' : game.name
+    }
+    const ul = li.appendChild(document.createElement('ul'))
+    ul.classList.add('inline')
+    for (const player of game.players) {
+      a = ul.appendChild(document.createElement('li'))
+      if (player.disconnected) {
+        a = a.appendChild(document.createElement('a'))
+        a.classList.add('disconnected')
+        a.onclick = () => {
+          if (gameInput.value === game.name && nameInput.value === player.name)
+            nameInput.value = ''
+          else {
+            gameInput.value = game.name
+            nameInput.value = player.name
+          }
+        }
+      }
+      a.textContent = player.name
+    }
+  }
+  gamesList.appendChild(fragment)
+  gamesList.hidden = !games.length
+})
+
+socket.on('updatePlayers', players => {
+  playersList.innerHTML = ''
+  for (const player of players) {
+    const li = fragment.appendChild(document.createElement('li'))
+    li.textContent = player.name
+    if (player.disconnected)
+      li.classList.add('disconnected')
+    if (player.current)
+      li.classList.add('current')
+    if (player.winner)
+      li.classList.add('winner')
+  }
+  playersList.appendChild(fragment)
+})
+
+socket.on('updateSpectators', spectators => {
+  spectatorsList.innerHTML = ''
+  let elem
+  if (spectators.length) {
+    spectators.unshift({ name: 'Spectators:' })
+  }
+  for (const spectator of spectators) {
+    elem = document.createElement('li')
+    elem.textContent = spectator.name
+    spectatorsList.appendChild(elem)
+  }
+})
+
+socket.on('joinedGame', data => {
+  gameInput.value = data.gameName
+  nameInput.value = data.playerName
+  spectateInput.checked = data.spectating
+  gameInput.disabled = true
+  nameInput.disabled = true
+  spectateInput.disabled = true
+  joinButton.hidden = true
+  if (!spectateInput.checked) {
+    spectateInput.previousElementSibling.hidden = true
+    spectateInput.hidden = true
+    startButton.hidden = false
+  }
+  errorMsg.innerHTML = ''
+  if (history.state === 'lobby')
+    history.pushState(data, `Game ${data.gameName}`)
+})
+
+socket.on('gameStarted', () => {
+  startButton.hidden = true
+  playArea.hidden = false
+  errorMsg.innerHTML = ''
+})
+
+socket.on('updateBoard', board => {
+  boardDiv.innerHTML = ''
+  for (let i = 0; i < board.length; i++) {
+    const piece = board[i]
+    const div = fragment.appendChild(document.createElement('div'))
+    div.classList.add('piece')
+    if (piece) {
+      for (let d = 0; d < 4; d++)
+        div.classList.add(`d${d}${piece.d[d]}`)
+      if (piece.arrow)
+        div.classList.add('arrow')
+    }
+    else {
+      div.classList.add('empty')
+      div.placeIndex = i
+    }
+  }
+  boardDiv.appendChild(fragment)
+  errorMsg.innerHTML = ''
+})
+
+socket.on('updatePieces', data => {
+  piecesDiv.innerHTML = ''
+  const current = !spectateInput.checked && nameInput.value === data.currentPlayer
+  for (let i = 0; i < data.pieces.length; i++) {
+    const piece = data.pieces[i]
+    const div = fragment.appendChild(document.createElement('div'))
+    div.pieceIndex = i
+    div.classList.add('piece')
+    for (let d = 0; d < 4; d++)
+      div.classList.add(`d${d}${piece.d[d]}`)
+    if (current) {
+      div.classList.add('selectable')
+      div.onclick = () => {
+        const selected = piecesDiv.querySelector('div.selected')
+        boardDiv.querySelectorAll('div.playable').forEach(div => {
+          div.classList.remove('playable')
+          div.onclick = null
+        })
+        if (selected) selected.classList.remove('selected')
+        if (selected !== div) {
+          div.classList.add('selected')
+          boardDiv.querySelectorAll('div.empty').forEach(placeDiv => {
+            placeDiv.classList.add('playable')
+            placeDiv.onclick = () => {
+              socket.emit('playRequest', { pieceIndex: div.pieceIndex, placeIndex: placeDiv.placeIndex })
+            }
+          })
+        }
+      }
+    }
+  }
+  piecesDiv.appendChild(fragment)
+  errorMsg.innerHTML = ''
+})
+
+socket.on('showUndo', show => {
+  if (!show) {
+    undoButton.hidden = true
+  }
+  else if (!spectateInput.checked) {
+    undoButton.hidden = false
+  }
+})
+
+socket.on('errorMsg', msg => {
+  errorMsg.innerHTML = msg
+})
