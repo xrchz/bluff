7151481af68daf6bb11d0c8faf05d19b25449881
Ramana Kumar <ramana@member.fsf.org>
2021-03-07 11:04:42 +0000

    Make client/config.js a template to be copied

--- client/config-template.js
+++ client/config-template.js
@@ -1,10 +1,10 @@
-const ServerPort = name => `/run/games/${name}.socket`
-const ServerURI = name => 'https://xrchz.net'
-const SocketOptions = name => ({path: `/games/${name}/socket.io`})
-/*
 const ServerPort = name => 12345
 const ServerURI = name => `http://localhost:${ServerPort(name)}`
 const SocketOptions = name => ({})
+/*
+const ServerPort = name => `/run/games/${name}.socket`
+const ServerURI = name => 'https://xrchz.net'
+const SocketOptions = name => ({path: `/games/${name}/socket.io`})
 */
 if (typeof exports !== 'undefined') {
   exports.ServerPort = ServerPort
