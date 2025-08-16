cbc9693d95025eae79f9f30cdf4b20bf5a6b461a
Ramana Kumar <ramana@member.fsf.org>
2025-08-16 11:27:53 +0000

    Fix for socket.rooms in unite

--- unite.js
+++ unite.js
@@ -312,8 +312,7 @@ io.on('connection', socket => {
     }
     else if (game.started) {
       if (game.players.find(player => player.name === socket.playerName && !player.socketId)) {
-        const rooms = Object.keys(socket.rooms)
-        if (rooms.length === 2 && rooms.includes(socket.id) && rooms.includes('lobby')) {
+        if (socket.rooms.size === 2 && socket.rooms.has(socket.id) && socket.rooms.has('lobby')) {
           console.log(`${socket.playerName} rejoining ${gameName}`)
           socket.gameName = gameName
           socket.leave('lobby'); socket.emit('updateGames', [])
